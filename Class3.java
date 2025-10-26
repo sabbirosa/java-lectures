e57054d5b96c04aa2451161fd3d4c9af5daff8a1
Sabbir Bin Abdul Latif <sabbir0727@gmail.com>
2025-10-26 17:20:41 +0600

    Refactor Class3: Remove initial greeting and add comments for loop examples

--- Class3.java
+++ Class3.java
@@ -1,5 +1,40 @@
 public class Class3 {
     public static void main(String[] args) {
-        System.out.println("Hello from Class3!");
+        
+        // for
+        // while 
+        // do-while
+        // int x = 5;
+        // int i = 6;
+        // Init value
+        // Condition
+        // Increment
+        // while (i < x) {
+        //     System.out.println(i);
+        //     // i++;
+        //     i = i + 1;
+        // }
+
+        // do {
+        //     System.out.println(i);
+        //     i = i + 1;
+        // } while (i < x);
+        String word = "jdfgDSaaAddofgd";
+        int c = 0;
+        int s = 0;
+        
+        // First get each and every charecter of the string
+        for (int i = 0; i < word.length(); i++) {
+            char x = word.charAt(i);
+
+            if (Character.isUpperCase(x)) {
+                c++;
+            } else {
+                s++;
+            }
+        }
+
+        System.out.println("Capital " + c);
+        System.out.println("Lower " + s);
     }
 }
